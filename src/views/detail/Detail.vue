3b7335c4d9bf6fbc9ed90c756e85bbc2cc7fa945
葛攀文 <1193535684@qq.com>
2020-12-31 15:38:10 +0800

    12.31

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>详情页</div>
+  <detail-nav-bar/>
 </template>
 
 <script>
+import DetailNavBar from "@/views/detail/childComps/DetailNavBar";
 export default {
   name: "Detail",
   data(){
     return{
       id:null
     }
+  },
+  components:{
+    DetailNavBar
   },
   created() {
     this.id = this.$route.params.id
---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <NavBar>
+    <div slot="left" class="back" @click="backClick">
+      <img src="~assets/img/common/back.svg" />
+    </div>
+    <div slot="center" class="nav">
+      <div v-for="(item,index) in titles"
+           class="nav-item"
+           :class="{active: index == currentIndex}"
+      @click="itemClick(index)">{{ item }}</div>
+    </div>
+  </NavBar>
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar";
+
+export default {
+  name: "DetailNavBar",
+  components: {NavBar},
+  comments: {
+    NavBar
+  },
+  data() {
+    return {
+      titles: ['商品', '参数', '评论', '推荐'],
+      currentIndex: 0
+    }
+  },
+  methods:{
+    itemClick(index){
+      this.currentIndex = index
+    },
+    backClick(){
+      this.$router.back();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.nav {
+  display: flex;
+  font-size: 14px;
+}
+
+.nav-item {
+  flex: 1;
+}
+.back img{
+  margin-top: 12px;
+}
+.active{
+  color: var(--color-high-text);
+}
+</style>
